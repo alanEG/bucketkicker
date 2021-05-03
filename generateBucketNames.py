6dda32de64c839a67d3bf366a3849e1fe1534ee7
abdallaEG <52584554+alanEG@users.noreply.github.com>
2021-05-03 19:18:24 +0200

    Update generateBucketNames.py

--- generateBucketNames.py
+++ generateBucketNames.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/python3 
 
 import sys
 
-if len(sys.argv) != 3:    
-    print "Usage: python generateBucketNames.py wordlistfile domainname"
+if len(sys.argv) != 3:
+    print("Usage: python generateBucketNames.py wordlistfile domainname")
     exit(1);
 
 with open(sys.argv[1]) as f:
     for line in f:
-        line=line.strip()
+        lines=line.strip()
         args=sys.argv[2].strip()
-    	print(line + "." + args)
-        print(line + "-" + args)
-        print(args + "-" + line)
-        
+        print(line.strip() + "." + args.strip())
+        print(lines.strip() + "-" + args.strip())
+        print(args.strip() + "-" + lines.strip())
+        print(arfs.strip() + lines.strip())
